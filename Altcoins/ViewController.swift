4af3acd96cd447e612ac9f38307756cbca41db56
Kuyawa <haxapp@gmail.com>
2017-04-27 10:48:22 -0400

    Initial Commit

---
+++ Altcoins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Altcoins
+//
+//  Created by Mac Mini on 4/27/17.
+//  Copyright © 2017 Armonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
